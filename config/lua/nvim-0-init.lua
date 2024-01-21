71e1800e4ad316d12d49c7e51140de567df84acf
Kit <kitherman@gmail.com>
2024-01-21 10:07:01 -0500

    color updates

--- config/lua/nvim-0-init.lua
+++ config/lua/nvim-0-init.lua
@@ -12,3 +12,26 @@ vim.o.history=500
 vim.o.cursorline=true
 vim.o.scrolloff=20
 vim.o.filetype = "on"
+vim.opt.ignorecase = true
+
+-- Define your colors
+local colors = {
+    darkgreen = "#005f00",
+    mediumgreen = "#008700",
+    lightgreen = "#00af00",
+    -- Add more colors as needed
+}
+
+-- Function to set highlight groups
+local function set_hl(group, opts)
+    vim.api.nvim_set_hl(0, group, opts)
+end
+
+-- Apply colors to highlight groups
+set_hl('Normal', { fg = colors.mediumgreen, bg = '#000000' })
+set_hl('Comment', { fg = colors.darkgreen})
+set_hl('CursorLine', { bg = '#080808' })
+set_hl('LineNr', { fg = colors.darkgreen })
+-- Custom highlight for Search
+set_hl('Search', { fg = '#1c1c1c', bg = colors.lightgreen })  -- dark gray closer to black
+
